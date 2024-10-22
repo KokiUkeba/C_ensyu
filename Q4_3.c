dcfb1c48da239fbf730bdfbcb43a4a0e9408b33e
KokiUkeba <234t2117@st.kumamoto-u.ac.jp>
2024-10-22 15:15:03 +0900

    remove some questions

--- Q4_3.c
+++ Q4_3.c
@@ -3,5 +3,25 @@
 
 int main(void)
 {
+    for (int i = 1; i <= 1000000; i++) {
+        if (!(i % 2) && (i != 2)) {
+            continue;
+        }
+
+        int j = 1;
+        while ((j += 2) <= sqrt(i)) {
+            if (!(i % j)) {
+                j = 0;
+                break;
+            }
+        }
+        
+        if (j == 0 || i == 1) {
+            continue;
+        }
+
+        printf("%d\n", i);
+    }
+
     return 0;
 }
